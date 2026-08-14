003b071a95c102d4ba082a501c94e64f092b54f0
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-14 14:25:10 -0500

    Add function to play a round
    Added the function to play a round and increase
    scores based on winner of round.

--- js/index.js
+++ js/index.js
@@ -17,3 +17,23 @@ function getHumanChoice() {
     let choice = prompt("Choose: rock, paper, or scissors"); 
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    let win = false;
+
+    if(humanChoice === computerChoice){
+        console.log("Tie!")
+    } else if(computerChoice === "rock" && humanChoice === "paper" 
+        || computerChoice === "paper" && humanChoice === "scissors"
+        || computerChoice === "scissors" && humanChoice === "rock") {
+            
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+}
+
