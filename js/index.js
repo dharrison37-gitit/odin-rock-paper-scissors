b8226a1086f85d57eb4b6f260117e6e34471f36e
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-14 14:27:52 -0500

    Add function to play whole game
    Added final function to play a whole game, which
    keeps track of scores and announces winner after
    completion.

--- js/index.js
+++ js/index.js
@@ -18,21 +18,46 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-
-    if(humanChoice === computerChoice){
-        console.log("Tie!")
-    } else if(computerChoice === "rock" && humanChoice === "paper" 
-        || computerChoice === "paper" && humanChoice === "scissors"
-        || computerChoice === "scissors" && humanChoice === "rock") {
-            
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-    } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+    
+        if(humanChoice === computerChoice){
+            console.log("Tie!")
+        } else if(computerChoice === "rock" && humanChoice === "paper" 
+            || computerChoice === "paper" && humanChoice === "scissors"
+            || computerChoice === "scissors" && humanChoice === "rock") {
+                
+                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    
     }
 
+    for(let i = 1; i < 5; i++) {
+        playRound(humanChoice, computerChoice);
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+    }
+
+    console.log(`Human: ${humanScore} | Computer: ${computerScore}`)
+    if(humanScore > computerScore) {
+        console.log("You have won the game!");
+    } else if (humanScore === computerScore) {
+        console.log("The game is a draw");
+    } else {
+        console.log("The computer has won the game.");
+    }
 }
 
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playGame();
