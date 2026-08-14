d93ea69ba452e8885cac16ebcc090b775bea193b
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-14 14:20:36 -0500

    Add method to get computer choice
    Added the method to get the computers choice for
    the game.
    
    Tested functionality with a console log. Everything
    appears to work.

---
+++ js/index.js
@@ -0,0 +1,14 @@
+"use srtict"
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else if (randomNumber === 3) {
+        return "scissors";
+    }
+}
