fde644f5e90195bab4250f934dfbc6368abe41b7
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-18 00:27:33 -0500

    Refactor most code
    Updated and removed a lot of code that was leading
    to confusion.
    
    Currently has a small bug, that requires one more
    click of a button before displaying game over and
    winner of game.

--- js/index.js
+++ js/index.js
@@ -12,57 +12,75 @@ function getComputerChoice() {
     }
 }
 
-// function getHumanChoice() {
-//     let choice = prompt("Choose: rock, paper, or scissors");
-//     return choice;
-// }
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let message = "";
-
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-
-        if (humanChoice === computerChoice) {
-            console.log("Tie!");
-        } else if (
-            (computerChoice === "rock" && humanChoice === "paper") ||
-            (computerChoice === "paper" && humanChoice === "scissors") ||
-            (computerChoice === "scissors" && humanChoice === "rock")
-        ) {
-            message = `You win! ${humanChoice} beats ${computerChoice}`;
-            // console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        } else {
-            message = `You lose! ${computerChoice} beats ${humanChoice}`;
-            // console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        gameResults.textContent = message;
-    }
-
-    const gameResults = document.querySelector("#game-results");
-    const playerControls = document.querySelector("#player-controls");
-    playerControls.addEventListener("click", (e) => {
-        playRound(e.target.id, getComputerChoice());
-        gameResults.textContent = message;
-    });
-
-    console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
-    if (humanScore > computerScore) {
-        message = "You have won the game!";
-        console.log("You have won the game!");
-    } else if (humanScore === computerScore) {
-        message = "The game is a draw";
-        console.log("The game is a draw");
+function getWinner(player, computer) {
+    if (player === computer) {
+        return "It's a tie game";
+    } else if (player > computer) {
+        return "You win!";
     } else {
-        message = "The computer has won the game";
-        console.log("The computer has won the game.");
+        return "Computer wins";
     }
 }
 
-// let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-playGame();
+function updateScoreBoard(winner) {
+    if (winner === "player") {
+        runningScores.textContent = playerScore;
+        gameResults.textContent = "Player wins!";
+    } else if (winner === "computer") {
+        runningScores.textContent = computerScore;
+        gameResults.textContent = "Computer wins!";
+    } else {
+        gameResults.textContent = "A tie";
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (!keepPlaying) return;
+    let roundWinner;
+    if (computerChoice === playerChoice) {
+        roundWinner = "none";
+    } else if (
+        (computerChoice === "rock" && playerChoice === "paper") ||
+        (computerChoice === "paper" && playerChoice === "scissors") ||
+        (computerChoice === "scissors" && playerChoice === "rock")
+    ) {
+        playerScore++;
+        roundWinner = "player";
+    } else {
+        computerScore++;
+        roundWinner = "computer";
+    }
+    updateScoreBoard(roundWinner);
+}
+
+function playGame(pChoice) {
+    let playerChoice = pChoice;
+    let computerChoice = getComputerChoice();
+    let message = "";
+    let scores = "";
+    let roundWinner = "";
+
+    if (playerScore < 5) {
+        playRound(playerChoice, computerChoice);
+    } else {
+        message = `Game Over - ${getWinner(playerScore, computerScore)}`;
+        keepPlaying = false;
+    }
+    choices.textContent = `Player chose: ${playerChoice} | Computer chose: ${computerChoice}`;
+    gameResults.textContent = message;
+    runningScores.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let keepPlaying = true;
+const gameResults = document.querySelector("#game-results");
+const runningScores = document.querySelector("#scores");
+const playerControls = document.querySelector("#player-controls");
+const choices = document.querySelector("#choices");
+playerControls.addEventListener("click", (e) => {
+    if (!keepPlaying) return;
+
+    let playerChoice = e.target.id;
+    playGame(playerChoice);
+});
